a5c71c11413cd51c6104101f8fd1a29f007ce27c
unknown <chenyan1020@live.com>
2016-09-19 19:27:26 -0400

    first commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Recipe()
+        {
+            ViewBag.Message = "Recipe";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact";
+
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            ViewBag.Message = "Gallery";
+
+            return View();
+        }
+    }
+}
